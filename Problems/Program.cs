0fc76b2df16c807e5234a83a88c27bb4701d5367
Nelly Cheboi <nellycheboi@gmail.com>
2019-02-05 13:51:10 -0600

    :ok_hand: Solved problem 3

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -11,10 +11,16 @@ namespace ProjectEuler
 		
 		public static void Main(string[] args)
 		{
-			Problems.EvenFibonacciNumbers();
+            // Establish an event handler to process key press events.
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(MyHandler);
 
-			// Keeps console open on debug mode
-			KeepConsoleOpen();
+            while (true)
+            {
+                Problems.LargestPrimeFactor();
+            }
+
+            // Keeps console open on debug mode
+            //KeepConsoleOpen();
 		}
 		public static void KeepConsoleOpen()
 		{
@@ -23,5 +29,22 @@ namespace ProjectEuler
 				Console.ReadLine();
 			#endif
 		}
-	}
+
+        protected static void MyHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.WriteLine("\nThe read operation has been interrupted.");
+
+            Console.WriteLine("  Key pressed: {0}", args.SpecialKey);
+
+            Console.WriteLine("  Cancel property: {0}", args.Cancel);
+
+            // Set the Cancel property to true to prevent the process from terminating.
+            Console.WriteLine("Setting the Cancel property to true...");
+            args.Cancel = true;
+
+            // Announce the new value of the Cancel property.
+            Console.WriteLine("  Cancel property: {0}", args.Cancel);
+            Console.WriteLine("The read operation will resume...\n");
+        }
+    }
 }
