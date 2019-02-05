825c9a58d3f2f8307d559648cba1495a0c8032e8
Nelly Cheboi <nellycheboi12@augustana.edu>
2019-02-05 06:23:10 -0600

    :tada: Solved problem one

---
+++ ProjectEuler/Problems.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+	public class Problems
+	{
+		public static void MultiplesOf3And5()
+		{
+			Console.WriteLine("Find the sum of all the multiples of 3 and 5 below a limit. Input a limit");
+			if(Int64.TryParse(Console.ReadLine(), out long limit))
+			{
+				long result = MultiplesOf3And5(limit);
+				Console.WriteLine($"The sum is {result}");
+			}
+			else
+			{
+				Console.WriteLine($"Invalid input. Make sure you input is an number. The maximum we can accept is {long.MaxValue}");
+			}
+		}
+
+		/// <summary>
+		/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+		/// Find the sum of all the multiples of 3 or 5 below 1000.
+		/// </summary>
+		/// <param name="limit"></param>
+		/// <returns></returns>
+		private static long MultiplesOf3And5(long limit)
+		{
+			
+			long currentSum = 0;
+			for(long i = 2; i <  limit; i++)
+			{
+				if(i%3==0 || i % 5 == 0)
+				{
+					currentSum += i;
+				}
+			}
+			return currentSum;
+		}
+
+	}
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+	class Program
+	{
+		
+		public static void Main(string[] args)
+		{
+			Problems.MultiplesOf3And5();
+
+			// Keeps console open on debug mode
+			KeepConsoleOpen();
+		}
+		public static void KeepConsoleOpen()
+		{
+			#if DEBUG
+				Console.WriteLine("Press enter to close...");
+				Console.ReadLine();
+			#endif
+		}
+	}
+}
